53cae0999c9d3799e30fc70138b8947289a3ae70
TruckLog1 <gabipatras19@gmail.com>
2026-05-11 18:12:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -172,7 +172,7 @@ function createWindow() {
   });
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.once('ready-to-show', () => {   mainWindow.show();   mainWindow.webContents.openDevTools(); });
 }
 
 app.whenReady().then(createWindow);
