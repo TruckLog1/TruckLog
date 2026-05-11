de0cc21bb11be887b1e2551a1a809efc6f7c9bc8
TruckLog1 <gabipatras19@gmail.com>
2026-05-11 13:56:19 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  saveReceipt: (filename, html) => ipcRenderer.invoke('save-receipt', { filename, html })
+  saveReceipt: (filename, html) => ipcRenderer.invoke('save-receipt', { filename, html }),
+  checkPlugin: () => ipcRenderer.invoke('check-plugin'),
+  autoInstallPlugin: () => ipcRenderer.invoke('auto-install-plugin'),
+  getTelemetry: () => ipcRenderer.invoke('get-telemetry')
 });
