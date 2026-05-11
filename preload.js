b3eb8a3e87ccb15675462b1d398dd518222c42c0
TruckLog1 <gabipatras19@gmail.com>
2026-05-11 16:30:13 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -4,5 +4,8 @@ contextBridge.exposeInMainWorld('electron', {
   saveReceipt: (filename, html) => ipcRenderer.invoke('save-receipt', { filename, html }),
   checkPlugin: () => ipcRenderer.invoke('check-plugin'),
   autoInstallPlugin: () => ipcRenderer.invoke('auto-install-plugin'),
-  getTelemetry: () => ipcRenderer.invoke('get-telemetry')
+  getTelemetry: () => ipcRenderer.invoke('get-telemetry'),
+  getAuthUrl: () => ipcRenderer.invoke('get-auth-url'),
+  getAuthServer: () => ipcRenderer.invoke('get-auth-server'),
+  onSteamAuth: (cb) => ipcRenderer.on('steam-auth', (_, url) => cb(url))
 });
